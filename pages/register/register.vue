14f16348c06034ac020e33d1a88fda5f8df67f37
zackyj <zackyjz@qq.com>
2021-08-20 11:25:00 +0800

    注册页面

---
+++ pages/register/register.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<image src="../../static/logo-2.png" mode="widthFix" class="register-logo"></image>
+		<view class="register-container">
+			<input type="text" placeholder="入职邀请码" class="register-code" 
+				maxlength="6" v-model="registerCode"/>
+				<view class="register-dsc">管理员为你创建工牌账号后，你可以从个人邮箱中获得注册邀请码</view>
+				<button class="register-btn" open-type="getUserInfo" @tap="register">现在注册</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("register.less");
+</style>
